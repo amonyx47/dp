bfd0a40e81f7c83fe1c35762bbe879c871710d91
Jan Minarciny <Jan.Minarciny1@ibm.com>
2017-11-04 14:07:25 +0100

    Initial commit

---
+++ src/main/java/sk/uniba/fmph/dp/DpApplication.java
@@ -0,0 +1,12 @@
+package sk.uniba.fmph.dp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DpApplication.class, args);
+    }
+}
